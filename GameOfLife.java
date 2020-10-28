a87de582eb1341b677cf42cbfc9a6e125342c132
JacobWeems <72574771+JacobWeems@users.noreply.github.com>
2020-10-28 23:58:21 +0000

    Added GameOfLife project
    I am going to start by building the simple game of life in java to help me learn java and to see if I find anything interesting on simulations for a simulation problem I am trying to solve. Later I may recreate this in C# when learning that a bit more for unity if this turns out to be a good learning process.

---
+++ GameOfLife.java
@@ -0,0 +1,13 @@
+public class GameOfLife {
+	public static void main(String[] args) {
+		int cols;
+		int rows;
+		boolean[][][] doubleGrid = new boolean[cols][rows][1];
+		
+		for (int i = 0; i < cols; i++) {
+			for (int j = 0; j < rows; j++) {
+				
+			}
+		}
+	}
+}
